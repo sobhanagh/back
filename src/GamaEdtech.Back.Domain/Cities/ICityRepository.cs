416867d08ce605cfb845fe734d6f23b5f015012f
Mohammad Taheri <taherimohammad08@gmail.com>
2025-01-14 14:32:06 +0330

    behavior: do not remove country if it has related states or cities

--- src/GamaEdtech.Back.Domain/Cities/ICityRepository.cs
+++ src/GamaEdtech.Back.Domain/Cities/ICityRepository.cs
@@ -7,6 +7,7 @@ public interface ICityRepository
 	public Task<City?> GetBy(Id id);
 	public Task<bool> ContainsCityWithNameInCountry(string name, Id countryId);
 	public Task<bool> ContainsCityWithNameInState(string name, Id stateId);
+	public Task<bool> ContainsCityInCountryWith(Id id);
 	public Task Add(City city);
 	public Task Remove(City city);
 }
