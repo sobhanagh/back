eaf01e3c51461a66bf8fe21dd5d3a4ef7a60f159
farsica <fariborz2004@yahoo.com>
2025-02-13 08:58:08 +0330

    fix:build error

--- src/ApplicationCore/Data/IUserId.cs
+++ src/ApplicationCore/Data/IUserId.cs
@@ -1,10 +1,10 @@
+#pragma warning disable IDE0040 // Add accessibility modifiers
 namespace GamaEdtech.Backend.Data
 {
-#pragma warning disable IDE0040 // Add accessibility modifiers
     public interface IUserId<TKey>
-#pragma warning restore IDE0040 // Add accessibility modifiers
     where TKey : IEquatable<TKey>
     {
         TKey UserId { get; set; }
     }
 }
+#pragma warning restore IDE0040 // Add accessibility modifiers
