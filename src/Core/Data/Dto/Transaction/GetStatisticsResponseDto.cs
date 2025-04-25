67f2b3b79dae996ee0b5582c52c989f7013d61df
farsica <fariborz2004@yahoo.com>
2025-04-25 19:22:33 +0330

    Group Transaction History by Date (Switch Between Day/Month) #174

--- src/Core/Data/Dto/Transaction/GetStatisticsResponseDto.cs
+++ src/Core/Data/Dto/Transaction/GetStatisticsResponseDto.cs
@@ -3,7 +3,7 @@ namespace GamaEdtech.Data.Dto.Transaction
     public sealed class GetStatisticsResponseDto
     {
         public string Name { get; set; }
-        public int Value { get; set; }
-        public bool IsDebit { get; set; }
+        public int DebitValue { get; set; }
+        public int CreditValue { get; set; }
     }
 }
